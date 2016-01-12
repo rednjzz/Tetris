dc8a82fd46d97fb567443fdb966965adaae1b976
rednjzz <rednjzz@gmail.com>
2016-01-12 15:00:30 +0900

    Initial Commit

---
+++ Tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by LeeKyuseop on 2016. 1. 12..
+//  Copyright © 2016년 rednjzz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
